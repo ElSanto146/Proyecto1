9c4cf2a61b00eab6191f1215d361f6985f4485f7
ElSanto146 <elsanto269@gmail.com>
2023-05-12 12:09:48 -0300

    Creación componentes
    Hasta el componente experiencia. seguir con educación

--- Proyecto1/src/app/app.module.ts
+++ Proyecto1/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    BannerComponent,
+    SobreMiComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
